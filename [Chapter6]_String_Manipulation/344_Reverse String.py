3794c78b284f53f33584e424fd0447b57d7d7a2a
JunnieLee <heunghari@naver.com>
2023-05-17 23:27:20 +0900

    feat: 344_reverse_string 내 풀이 업로드

--- [Chapter6]_String_Manipulation/344_Reverse String.py
+++ [Chapter6]_String_Manipulation/344_Reverse String.py
@@ -1,1 +1,17 @@
 # https://leetcode.com/problems/reverse-string/
+
+# 내 풀이 --------------------------------------------------------------------
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        length = len(s)
+        half = length//2
+        for i in range(half):
+            front = s[i]
+            s[i] = s[length-1-i]
+            s[length-1-i] = front
+
+# 교재 풀이 --------------------------------------------------------------------
