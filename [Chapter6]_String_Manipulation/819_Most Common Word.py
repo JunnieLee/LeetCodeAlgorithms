e778482c991c6c0dd72e22632186b82737779135
JunnieLee <heunghari@naver.com>
2023-05-18 15:41:11 +0900

    feat: 819 most common word 내 수정된 풀이 업로드

--- [Chapter6]_String_Manipulation/819_Most Common Word.py
+++ [Chapter6]_String_Manipulation/819_Most Common Word.py
@@ -6,15 +6,14 @@ import re
 
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        s = paragraph.lower().replace(',', ' ')
+        s = re.sub(r'[^a-z]', ' ', paragraph.lower())
         # input "a, a, a, a, b,b,b,c, c" 일때 때문에 replace 넣어줌
         arr = s.split()
         frequency_dict = {}
         for element in arr:
-            e = re.sub(r'[^a-z]', '', element)
-            if (e not in banned):
-                if e in frequency_dict:
-                    frequency_dict[e] +=1
+            if (element not in banned):
+                if element in frequency_dict:
+                    frequency_dict[element] +=1
                 else:
-                    frequency_dict[e] =1
-        return max(frequency_dict, key=frequency_dict.get) # value를 key로 해서 max value를 지닌 key를 return   
+                    frequency_dict[element] =1
+        return max(frequency_dict, key=frequency_dict.get) # value를 key로 해서 max value를 지닌 key를 return  
